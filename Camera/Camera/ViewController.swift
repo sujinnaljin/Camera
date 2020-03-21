e4c71e73063aecfa84b39fe82f496171d2cd858e
강수진 <rkdthd1234@naver.com>
2020-03-21 16:36:18 +0900

    Create Initial Project

---
+++ Camera/Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 강수진 on 2020/03/21.
+//  Copyright © 2020 강수진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
